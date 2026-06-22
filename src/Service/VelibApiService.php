120a2a12a2388b012fd6d87820c67989eb89f5da
christophe Gobet <christophe.gpa@gmail.com>
2026-06-22 21:17:17 +0200

    Integrate Velib Station status API

--- src/Service/VelibApiService.php
+++ src/Service/VelibApiService.php
@@ -2,21 +2,29 @@
 
 namespace App\Service;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class VelibApiService
 {
+    public function __construct(
+        private HttpClientInterface $httpClient
+    ) {
+    }
+
     public function getStations(): array
     {
-        return [
-            [
-                'name' => 'Gare de Lyon',
-                'bikes' => 12,
-                'stands' => 8,
-            ],
-            [
-                'name' => 'Bastille',
-                'bikes' => 5,
-                'stands' => 15,
-            ],
-        ];
+        $response = $this->httpClient->request(
+            'GET',
+            'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json'
+        );
+
+        $data = $response->toArray();
+
+        return array_slice(
+            $data['data']['stations'],
+            0,
+            20
+        );
     }
 }
+
