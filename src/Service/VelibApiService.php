3d96c61b34a8c88cbe9483f52b2875b2b0f40d0b
christophe Gobet <christophe.gpa@gmail.com>
2026-06-22 21:27:53 +0200

    Display real Velib station with Bootstrap cards

--- src/Service/VelibApiService.php
+++ src/Service/VelibApiService.php
@@ -6,25 +6,58 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class VelibApiService
 {
+    private const STATUS_URL =
+        'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json';
+
+    private const INFO_URL =
+        'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json';
+
+
     public function __construct(
         private HttpClientInterface $httpClient
     ) {
     }
 
+
     public function getStations(): array
     {
-        $response = $this->httpClient->request(
-            'GET',
-            'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json'
-        );
+        $status = $this->httpClient
+            ->request('GET', self::STATUS_URL)
+            ->toArray();
 
-        $data = $response->toArray();
 
-        return array_slice(
-            $data['data']['stations'],
-            0,
-            20
-        );
+        $information = $this->httpClient
+            ->request('GET', self::INFO_URL)
+            ->toArray();
+
+
+        $stationsInfo = [];
+
+        foreach ($information['data']['stations'] as $station) {
+            $stationsInfo[$station['station_id']] = $station;
+        }
+
+
+        $stations = [];
+
+        foreach ($status['data']['stations'] as $station) {
+
+            $id = $station['station_id'];
+
+            $stations[] = [
+                'name' => $stationsInfo[$id]['name'] ?? 'Station inconnue',
+                'address' => $stationsInfo[$id]['address'] ?? '',
+                'latitude' => $stationsInfo[$id]['lat'] ?? null,
+                'longitude' => $stationsInfo[$id]['lon'] ?? null,
+
+                'bikes' => $station['num_bikes_available'],
+                'docks' => $station['num_docks_available'],
+            ];
+        }
+
+
+        return array_slice($stations, 0, 20);
     }
 }
+
 
